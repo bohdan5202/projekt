85fb3c7a84ea2d0caf820d920968c6c47304530b
bohdan5202 <kramar24155@stud.akademiabialska.pl>
2026-03-11 00:26:51 +0100

    update main.py to visualize top 10 best-selling games

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('best_selling_video_games.csv')
-mask = df['Rank'].apply(lambda x: not str(x).isdigit())
-df.loc[mask,"Publisher(s)"] = df.loc[mask, "Releaseyear"]
-df.loc[mask, "Releaseyear"] = df.loc[mask, "Platform(s)"]
-df.loc[mask, "Platform(s)"] = df.loc[mask, "Series"]
-df.loc[mask, "Series"] = df.loc[mask, "Rank"]
-temp = df.loc[mask, "Title"]
-df.loc[mask, "Title"] = df.loc[mask, "Sales(millions)"]
-df.loc[mask, "Sales(millions)"] = temp
-df['Rank'] = df.index + 1 
-new_mask = df['Series'].str.contains(r"\[.*?\]")
-df.loc[new_mask, "Ref."] = df.loc[new_mask, "Series"].str.extract(r'(\[.*?\])').iloc[:,0]
-df.loc[new_mask, "Series"] = df.loc[new_mask, "Series"].str.replace(r"\[.*?\]", "", regex=True).str.strip()
-print(df.loc[new_mask])
-print(df['Title'].dtype)
-print(df[["Sales(millions)", "Releaseyear"]].describe())
-df.fillna({"Ref.": "[#]"}, inplace=True)
+# df = pd.read_csv('best_selling_video_games.csv')
+# mask = df['Rank'].apply(lambda x: not str(x).isdigit())
+# df.loc[mask,"Publisher(s)"] = df.loc[mask, "Releaseyear"]
+# df.loc[mask, "Releaseyear"] = df.loc[mask, "Platform(s)"]
+# df.loc[mask, "Platform(s)"] = df.loc[mask, "Series"]
+# df.loc[mask, "Series"] = df.loc[mask, "Rank"]
+# temp = df.loc[mask, "Title"]
+# df.loc[mask, "Title"] = df.loc[mask, "Sales(millions)"]
+# df.loc[mask, "Sales(millions)"] = temp
+# df['Rank'] = df.index + 1 
+# new_mask = df['Series'].str.contains(r"\[.*?\]")
+# df.loc[new_mask, "Ref."] = df.loc[new_mask, "Series"].str.extract(r'(\[.*?\])').iloc[:,0]
+# df.loc[new_mask, "Series"] = df.loc[new_mask, "Series"].str.replace(r"\[.*?\]", "", regex=True).str.strip()
+# print(df.loc[new_mask])
+# print(df['Title'].dtype)
+# print(df[["Sales(millions)", "Releaseyear"]].describe())
+# df.fillna({"Ref.": "[#]"}, inplace=True)
+# df.sort_values(by="Sales(millions)", ascending=False, inplace=True)
+# print(df["Ref."].value_counts())
+# print(df.head(10))
+# print(df['Publisher(s)'].value_counts())
+# print(df.info())
+# print(df[df["Publisher(s)"] == "Nintendo"])
+# mask_name = df[df['Series'].isna()].index
+# df.loc[mask_name, "Series"] = df.loc[mask_name, "Title"]
+# print(df.loc[mask_name])
+# df.to_csv('cleaned_data.csv', index=False)
+# print(df.info())
+df = pd.read_csv('clear.csv')
 df.sort_values(by="Sales(millions)", ascending=False, inplace=True)
-print(df["Ref."].value_counts())
-print(df.head(10))
-print(df['Publisher(s)'].value_counts())
-print(df.info())
-print(df[df["Publisher(s)"] == "Nintendo"])
-mask_name = df[df['Series'].isna()].index
-df.loc[mask_name, "Series"] = df.loc[mask_name, "Title"]
-print(df.loc[mask_name])
-df.to_csv('cleaned_data.csv', index=False)
-print(df.info())
+top10 = df.head(10)
+top10.plot(x="Title", y="Sales(millions)", kind="bar", legend=False)
+plt.title("Top 10 Best-Selling Video Games")
+plt.xlabel("Title")
+plt.ylabel("Sales (millions)")
+plt.xticks(rotation=45, ha="right")
+plt.tight_layout()
+plt.savefig("top_10_pandas.png")
